ce582a38bc14c1ca941b907294c23f7ef468374f
Sofia Bueno Silva <sbueno-s@c1r12s9.42porto.com>
2024-11-18 16:45:36 +0000

    philo

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,29 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sofiabueno <sofiabueno@student.42.fr>      +#+  +:+       +#+        */
+/*   By: sbueno-s <sbueno-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 17:13:59 by sofiabueno        #+#    #+#             */
-/*   Updated: 2024/08/22 15:10:04 by sofiabueno       ###   ########.fr       */
+/*   Updated: 2024/08/23 15:46:49 by sbueno-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
-
-int	check_param(char **av)
-{
-	if (atoi(av[1]) <= 0 || !is_number(av[1]))
-		return (write(2, "Wrong number_of_philosophers\n", 30), 1);
-	if (atoi(av[2]) <= 0 || !is_number(av[2]))
-		return (write(2, "Wrong time_to_die\n", 19), 1);
-	if (atoi(av[3]) <= 0 || !is_number(av[3]))
-		return (write(2, "Wrong time_to_eat\n", 19), 1);
-	if (atoi(av[4]) <= 0 || !is_number(av[4]))
-		return (write(2, "Wrong time_to_sleep\n", 21), 1);
-	if (av[5] && (atoi(av[5]) <= 0 || !is_number(av[5])))
-		return (write(2, "Wrong num_of_times_philosophers_must_eat\n", 42), 1);
-	return (0);
-}
 
 int	main(int ac, char **av)
 {
@@ -33,6 +18,7 @@ int	main(int ac, char **av)
 		ft_exit("Wrong number of arguments");
 	if (!check_param(av))
 		return (EXIT_FAILURE);
+	init_structs();
 	return (0);
 }
 
---
+++ philo/src/philo.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbueno-s <sbueno-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 15:33:19 by sbueno-s          #+#    #+#             */
+/*   Updated: 2024/08/23 15:46:43 by sbueno-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	check_param(char **av)
+{
+	if (atoi(av[1]) <= 0 || !is_number(av[1]))
+		return (write(2, "Wrong number_of_philosophers\n", 30), 1);
+	if (atoi(av[2]) <= 0 || !is_number(av[2]))
+		return (write(2, "Wrong time_to_die\n", 19), 1);
+	if (atoi(av[3]) <= 0 || !is_number(av[3]))
+		return (write(2, "Wrong time_to_eat\n", 19), 1);
+	if (atoi(av[4]) <= 0 || !is_number(av[4]))
+		return (write(2, "Wrong time_to_sleep\n", 21), 1);
+	if (av[5] && (atoi(av[5]) <= 0 || !is_number(av[5])))
+		return (write(2, "Wrong num_of_times_philosophers_must_eat\n", 42), 1);
+	return (0);
+}
+void	init_philo
